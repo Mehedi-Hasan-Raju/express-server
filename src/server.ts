9e8dabd04069e870959323b687d0b4e752da361d
Mehedi-Hasan-Raju <tecmehedi7@gmail.com>
2026-07-08 12:36:07 +0600

    get all user & singel user created

--- src/server.ts
+++ src/server.ts
@@ -49,15 +49,72 @@ app.use(express.json());
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello Raju!')
 })
+//user CRUD
+app.post('/users', async (req:Request, res:Response)=>{
+    const {name, email} = req.body;
 
-app.post('/', (req:Request, res:Response)=>{
-   console.log(req.body);
-
-   res.status(201).json({
-    success: true,
-    message: 'Data submitted successfully'
-   });
+    try{
+        const result = await pool.query(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`,[name, email]);
+        // console.log(result.rows[0]);
+        
+        res.status(201).json({
+            success: true,
+            message: 'data inserted successfully',
+            data: result.rows[0]
+        })
+       
+        
+    }catch(err: any){
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
 });
+
+app.get('/users', async(req:Request, res:Response)=> {
+    try{
+        const result = await pool.query(`SELECT * FROM users`);
+
+        res.status(200).json({
+            success: true,
+            message: 'user data fetched successfully',
+            data: result.rows,
+        })
+
+    }catch(err: any){
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err,
+        })
+    }
+})
+
+app.get('/users/:id', async(req : Request, res: Response) =>{
+    console.log(req.params.id);
+   try{
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [req.params.id]);
+    if(result.rows.length === 0 ){
+        res.status(404).json({
+            success: false,
+            message: 'user not found',
+        })
+    }else{
+        res.status(200).json({
+            success: true,
+            message: 'user data fetched successfully',
+            data: result.rows[0]
+        })
+    }
+   }catch(err:any){
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err,
+        })
+   }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
