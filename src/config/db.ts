ea8ed136f31a2a338c6270b590f5a44804f8dc85
Mehedi-Hasan-Raju <tecmehedi7@gmail.com>
2026-07-10 16:20:23 +0600

    configuration folder create

---
+++ src/config/db.ts
@@ -0,0 +1,35 @@
+import { Pool } from 'pg';
+import config from '.';
+//DB
+export const pool = new Pool({
+    connectionString: `${config.connectionString}`,
+})
+
+const initDB = async () => {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            age INT,
+            phone VARCHAR(20),
+            address TEXT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )`);
+
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS todos(
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id) ON DELETE CASCADE,
+            title VARCHAR(255) NOT NULL,
+            description TEXT,
+            completed BOOLEAN DEFAULT false,
+            due_date DATE,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )`);
+};
+  
+
+export default initDB;
--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,16 @@
 import express, {Request, Response } from 'express';
-import{Pool} from 'pg';
-import dotenv from 'dotenv';
-import path from 'path/win32';
+import initDB, { pool } from './config/db';
+import config from './config';
 
-dotenv.config({path: path.join(process.cwd(), ".env" )});
+
+
 
 const app = express()
-const port = 5000
+const port = config.port;
 
 
-//DB
-const pool = new Pool({
-    connectionString: `${process.env.CONNECTION_STR}`,
-})
 
-const initDB = async () => {
-    await pool.query(`
-        CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            email VARCHAR(255) NOT NULL UNIQUE,
-            age INT,
-            phone VARCHAR(20),
-            address TEXT,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )`);
-
-        await pool.query(`
-            CREATE TABLE IF NOT EXISTS todos(
-            id SERIAL PRIMARY KEY,
-            user_id INT REFERENCES users(id) ON DELETE CASCADE,
-            title VARCHAR(255) NOT NULL,
-            description TEXT,
-            completed BOOLEAN DEFAULT false,
-            due_date DATE,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )`);
-};
-
+//initialize database
 initDB();
 //loger middleware
 
